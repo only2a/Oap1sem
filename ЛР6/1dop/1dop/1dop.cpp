d474b2156f6965811dc0a8bceeaf1e4d6a90d0b0
Dima <t5p.2a@mail.ru>
2021-09-04 11:31:42 +0300

    oap1sem

---
+++ ЛР6/1dop/1dop/1dop.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    int a = 100, b, c, d; double s;
+    while (a <= 999) {
+    b = a / 100;
+    c = (a - b * 100) / 10;
+    d = (a - b * 100 - c * 10);
+    s= sqrt(a);
+   
+        if ((c > b) && (c < d) && (fmod(a, s) == 0)) {cout << a << endl; break;
+    }
+  
+          a++;
+    }
+    return(0);
+    
+}
+
---
+++ ЛР8/8.3(11)/8.3(11)/8.3(11).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	double a, b, e = 0.0001, x1, x;
+
+	cout << "Введите a:"; cin >> a; cout << "Введите b: "; cin >> b;
+
+
+	((pow(a, 3) + sin(a)) * (3 * pow(a, 2) + cos(a))) > 0 ? x1 = a : x1 = b;
+
+	do {
+		x = x1;
+		x1 = x - (pow(x, 3) + sin(x)) / (3 * pow(x, 2) + cos(x));
+	} while (abs(x1 - x) > e);
+
+	cout << "х1 = " << x1 << endl;
+}
